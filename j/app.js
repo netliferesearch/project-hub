5285ce7bfcdbe36d2f1aa36bc5a72ff4a0f8dfb9
Raymond Julin <raymond.julin@gmail.com>
2015-05-01 09:52:18 -0400

    Dynamic list in angular

---
+++ j/app.js
@@ -0,0 +1,28 @@
+angular.module('ProjectHub', [])
+    .controller('Hub', function($scope, $http, $sce) {
+        var url = 'http://45.55.218.128/events';
+        $scope.e = {};
+        $scope.events = [];
+        console.log('a');
+        var update = function() {
+            $http.get(url).then(function(it) {
+                console.log(it);
+                $scope.events = it.data.map(function(i) {
+                    i.future = i.date.match(/2016/);
+                    i.body = $sce.trustAsHtml(i.body);
+                    return i;
+                });
+            });
+        };
+
+        update();
+        $scope.submitForm = function() {
+            var data = {
+                title: $scope.e.title,
+                date: $scope.e.date,
+                body: $('#redactor').redactor('code.get'),
+                type: $scope.e.type,
+            };
+            $http.post(url, data).then(update);
+        }
+    })
